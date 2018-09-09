ed40be92143ceb8956d96f09ac28ed841cf42827
xingyannian <xingyannian@163.com>
2018-09-09 21:46:15 +0800

    新建项目

---
+++ src/main/java/com/xingyannian/deyu/DeyuApplication.java
@@ -0,0 +1,12 @@
+package com.xingyannian.deyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeyuApplication.class, args);
+	}
+}
